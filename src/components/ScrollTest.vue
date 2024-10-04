4a618d14b64eb0222c893d1238d52b84c69c364b
Vincent Gustafsson <vincent@Vincents-MacBook-Air.local>
2024-10-04 15:27:10 +0200

    ?

--- src/components/ScrollTest.vue
+++ src/components/ScrollTest.vue
@@ -14,8 +14,20 @@
     <div style="display: flex; overflow-x: auto; overflow-y: hidden; margin: 20px 0;">
 
         <div class="it" v-for="user in props.users">
-            <v-card :to="{ name: 'user', params: { id: String(user.id) } }" link width="300" height="200">
+            <v-card
+                :to="{ name: 'user', params: { id: String(user.id) } }"
+                link
+                width="300"
+                height="200"
+            >
+            <v-img
+                class="align-end text-white"
+                height="200"
+                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                cover
+                >
                 <v-card-title>{{ user.username }}</v-card-title>
+                </v-img>
             </v-card>
         </div>
     </div>
